19898721bd173e4626394582d8db887ab2d70457
stehlib <stehlib@amazon.com>
2017-06-20 08:41:36 -0700

    Initial Commit

---
+++ DataDataData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataDataData
+//
+//  Created by Stehling, Brennan on 6/20/17.
+//  Copyright © 2017 Acme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
